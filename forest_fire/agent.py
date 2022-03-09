eb6f8835c2483542099f17ae10c4bf4143398d35
Jônatas G <jonatasgeek@gmail.com>
2022-03-08 21:51:58 -0300

    first commit

---
+++ forest_fire/agent.py
@@ -0,0 +1,41 @@
+from mesa import Agent
+from random import randint
+
+
+class TreeCell(Agent):
+    """
+    A tree cell.
+
+    Attributes:
+        x, y: Grid coordinates
+        condition: Can be "Fine", "On Fire", or "Burned Out"
+        unique_id: (x,y) tuple.
+
+    unique_id isn't strictly necessary here, but it's good
+    practice to give one to each agent anyway.
+    """
+
+    def __init__(self, pos, model, intensity=50):
+        """
+        Create a new tree.
+        Args:
+            pos: The tree's coordinates on the grid.
+            model: standard model reference for agent.
+        """
+        super().__init__(pos, model)
+        self.pos = pos
+        self.condition = "Fine"
+        self.intensity = intensity
+
+    def step(self):
+        """
+        If the tree is on fire, spread it to fine trees nearby.
+        """
+        if self.condition == "On Fire":
+            for neighbor in self.model.grid.neighbor_iter(self.pos):
+                if neighbor.condition in ["Fine", "Escaped the Fire"]:
+                    if randint(1,100) < self.intensity:
+                        neighbor.condition = "On Fire"
+                    else:
+                        neighbor.condition = "Escaped the Fire"
+            self.condition = "Burned Out"
---
+++ forest_fire/model.py
@@ -0,0 +1,77 @@
+from mesa import Model
+from mesa.datacollection import DataCollector
+from mesa.space import Grid
+from mesa.time import RandomActivation
+
+from .agent import TreeCell
+
+
+class ForestFire(Model):
+    """
+    Simple Forest Fire model.
+    """
+
+    def __init__(self, width=100, height=100, density=0.65, start_pos_x=0, start_pos_y=0, intensity=80):
+        """
+        Create a new forest fire model.
+
+        Args:
+            width, height: The size of the grid to model
+            density: What fraction of grid cells have a tree in them.
+        """
+        # Set up model objects
+        self.schedule = RandomActivation(self)
+        self.grid = Grid(width, height, torus=False)
+        self.density = density
+        self.start_pos_x = start_pos_x
+        self.start_pos_y = start_pos_y
+        self.intensity = intensity
+
+        self.datacollector = DataCollector(
+            {
+                "Fine": lambda m: self.count_type(m, "Fine"),
+                "On Fire": lambda m: self.count_type(m, "On Fire"),
+                "Burned Out": lambda m: self.count_type(m, "Burned Out"),
+            }
+        )
+
+        # Place a tree in each cell with Prob = density
+        for (contents, x, y) in self.grid.coord_iter():
+            if self.random.random() < density:
+                # Create a tree
+                new_tree = TreeCell((x, y), self, intensity)
+                # Set all trees in the first column on fire.
+                if x >= start_pos_x + - 1 and x <= start_pos_x + 1  and y >= start_pos_y + -1 and y <= start_pos_y + 1:
+                    new_tree.condition = "On Fire"
+                self.grid._place_agent((x, y), new_tree)
+                self.schedule.add(new_tree)
+
+        self.running = True
+        self.datacollector.collect(self)
+
+    def step(self):
+        """
+        Advance the model by one step.
+        """
+        self.schedule.step()
+        # collect data
+        self.datacollector.collect(self)
+
+        # Halt if no more fire
+        if self.count_type(self, "On Fire") == 0:
+            self.running = False
+            forest_agent = self.datacollector.get_agent_vars_dataframe()
+            forest_model = self.datacollector.get_model_vars_dataframe()
+            forest_agent.to_csv('forest_agent_density_' + str(self.density) + '_start-pos-x_' + str(self.start_pos_x) + '_start-pos-y_' + str(self.start_pos_y) + '.csv')
+            forest_model.to_csv('forest_model_density_' + str(self.density) + '_start-pos-x_' + str(self.start_pos_x) + '_start-pos-y_' + str(self.start_pos_y) + '.csv')
+
+    @staticmethod
+    def count_type(model, tree_condition):
+        """
+        Helper method to count trees in a given condition in a given model.
+        """
+        count = 0
+        for tree in model.schedule.agents:
+            if tree.condition == tree_condition:
+                count += 1
+        return count
